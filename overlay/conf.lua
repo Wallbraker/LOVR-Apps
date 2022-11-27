32fc219b4d6b5a977d0926840937dae983d03f23
Jakob Bornecrantz <jakob@collabora.com>
2022-11-27 13:39:26 +0000

    overlay: Better setup window

--- overlay/conf.lua
+++ overlay/conf.lua
@@ -1,6 +1,13 @@
 function lovr.conf(t)
 	t.headset.drivers = {'openxr'}
 	t.headset.overlay = true
-	t.window.resizable = true
-	-- t.window = nil
+
+	if false or t.window == nil then
+		t.window = nil
+	else
+		t.window.width = 800
+		t.window.height = 600
+		t.window.resizable = true
+		t.window.title = "Overlay Test"
+	end
 end
