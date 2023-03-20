45c9912f3f9b6e0f1ac2071b5f9cce97db891745
Jakob Bornecrantz <jakob@collabora.com>
2023-03-20 01:14:15 +0000

    overlay: Draw more information at grip and aim poses

--- overlay/helpers.lua
+++ overlay/helpers.lua
@@ -61,7 +61,7 @@ end
 
 ----
 -- Helper to draw a axis cross at a device pose.
-function drawAxis(pass, device, length)
+function drawAxis(pass, device, stop, start)
 	if not lovr.headset.isTracked(device) then return end
 
 	local vec3 = lovr.math.vec3
@@ -71,29 +71,61 @@ function drawAxis(pass, device, length)
 	local rot = lovr.math.newQuat(a, ax, ay, az)
 
 	local center = vec3(x, y, z)
-	local forward = rot:direction():mul(length)
-	local up = rot:mul(vec3(0, 1, 0)):mul(length)
-	local right = rot:mul(vec3(1, 0, 0)):mul(length)
+
+	start = start or 0.0
+
+	local right = rot:mul(vec3(1, 0, 0))
+	local right_start = right * start
+	local right_stop = right * stop
+
+	local forward = rot:direction()
+	local forward_start = forward * start
+	local forward_stop = forward * stop
+
+	local up = rot:mul(vec3(0, 1, 0))
+	local up_start = up * start
+	local up_stop = up * stop
 
 	local n1 = 0.0
 	local n2 = 0.5
 	local p1 = 0.2
 	local p2 = 1.0
+	local f = up * 0.1
 
 	pass:setColor(p2, p1, p1)
-	pass:line(center, center + right)
+	pass:line(center + right_start, center + right_stop)
 	pass:setColor(n2, n1, n1)
-	pass:line(center, center - right)
+	pass:line(center - right_start, center - right_stop)
 
 	pass:setColor(p1, p2, p1)
-	pass:line(center, center + up)
+	pass:line(center + up_start, center + up_stop)
 	pass:setColor(n1, n2, n1)
-	pass:line(center, center - up)
+	pass:line(center - up_start, center - up_stop)
 
 	pass:setColor(p1, p1, p2)
-	pass:line(center, center + forward)
+	pass:line(center + forward_start, center + forward_stop)
 	pass:setColor(n1, n1, n2)
-	pass:line(center, center - forward)
+	pass:line(center - forward_start, center - forward_stop)
+end
+
+----
+-- Draws text at the location of the given device
+-- but rotated so it is always facing the user.
+function drawTextAtDeviceLookingAtUser(pass, device, text, scale)
+	if not lovr.headset.isTracked(device) then return end
+
+	scale = scale or 0.1
+	text = text or device
+
+	local vec3 = lovr.math.vec3
+	local user = vec3(lovr.headset.getPosition())
+	local pos = vec3(lovr.headset.getPosition(device))
+
+	local view = lovr.math.newMat4():target(pos, user, vec3(0, 1, 0))
+	view = view:scale(vec3(-scale, scale, scale))
+
+	pass:setColor(1, 1, 1)
+	pass:text(text, view)
 end
 
 ----
--- overlay/main.lua
+++ overlay/main.lua
@@ -61,11 +61,15 @@ function renderScene(pass, isMirror)
 	local draw_axis = true
 
 	if draw_axis then
-		drawAxis(pass, 'hand/left', 0.1)
-		drawAxis(pass, 'hand/left/point', 0.1)
+		drawAxis(pass, 'hand/left', 0.1, 0.01)
+		drawTextAtDeviceLookingAtUser(pass, 'hand/left', 'grip', 0.01)
+		drawAxis(pass, 'hand/left/point', 0.1, 0.01)
+		drawTextAtDeviceLookingAtUser(pass, 'hand/left/point', 'aim', 0.01)
 
-		drawAxis(pass, 'hand/right', 0.1)
-		drawAxis(pass, 'hand/right/point', 0.1)
+		drawAxis(pass, 'hand/right', 0.1, 0.01)
+		drawTextAtDeviceLookingAtUser(pass, 'hand/right', 'grip', 0.01)
+		drawAxis(pass, 'hand/right/point', 0.1, 0.01)
+		drawTextAtDeviceLookingAtUser(pass, 'hand/right/point', 'aim', 0.01)
 	else
 		pass:setColor(1, 1, 1)
 		drawLineZ(pass, 'hand/left/point', 1.0)
