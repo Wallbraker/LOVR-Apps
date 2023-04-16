bc12c6991ea00dc2e3bc276f7078fb8185511234
Jakob Bornecrantz <jakob@collabora.com>
2023-04-16 15:42:21 +0100

    scripts: Update scripts to build AppImages for each project

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -2,4 +2,6 @@
 
 docker run --name temp-lovr-apps lovr-apps:latest
 docker cp temp-lovr-apps:/root/apps/LÖVR-x86_64.AppImage .
+docker cp temp-lovr-apps:/root/apps/OverlayTest-x86_64.AppImage .
+docker cp temp-lovr-apps:/root/apps/HandVisualizer-x86_64.AppImage .
 docker rm temp-lovr-apps
